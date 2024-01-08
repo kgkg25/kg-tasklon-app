9eacdbb3338a6eb237f33036af138a07310de3dc
kgkg25 <kgkg@i.ua>
2024-01-08 09:43:59 +0100

    first commit

---
+++ app/models/QRCode.server.js
@@ -0,0 +1,42 @@
+import qrcode from "qrcode";
+import invariant from "tiny-invariant";
+import db from "../db.server";
+
+export async function getQRCode(id, graphql) {
+    const qrCode = await db.qRCode.findFirst({ where: { id } });
+
+    if (!qrCode) {
+        return null;
+    }
+
+    return supplementQRCode(qrCode, graphql);
+}
+  
+export async function getQRCodes(shop, graphql) {
+    const qrCodes = await db.qRCode.findMany({
+        where: { shop },
+        orderBy: { id: "desc" },
+    });
+
+    if (qrCodes.length === 0) return [];
+
+    return Promise.all(
+        qrCodes.map((qrCode) => supplementQRCode(qrCode, graphql))
+    );
+}
+
+export function getQRCodeImage(id) {
+    const url = new URL(`/qrcodes/${id}/scan`, process.env.SHOPIFY_APP_URL);
+    return qrcode.toDataURL(url.href);
+}
+
+export function getDestinationUrl(qrCode) {
+    if (qrCode.destination === "product") {
+      return `https://${qrCode.shop}/products/${qrCode.productHandle}`;
+    }
+  
+    const match = /gid:\/\/shopify\/ProductVariant\/([0-9]+)/.exec(qrCode.productVariantId);
+    invariant(match, "Unrecognized product variant ID");
+  
+    return `https://${qrCode.shop}/cart/${match[1]}:1`;
+}
